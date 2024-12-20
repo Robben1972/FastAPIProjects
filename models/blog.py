fe8210c5b15af988c0553c4659a1ab8cd441b1f1
Shakhobiddin Bozorov <gravitifolz.01@gmail.com>
2024-12-20 11:17:40 +0500

    model changing

--- models/blog.py
+++ models/blog.py
@@ -12,14 +12,14 @@ class Blog(Base):
     created = Column(DateTime(timezone=True), server_default=func.now())
     updated = Column(DateTime(timezone=True), onupdate=func.now())
 
-    comments = relationship('Comment', back_populates='blog')
+    comments = relationship('Comment', back_populates='blog', cascade='all, delete-orphan')
 
 # Comment Model
 class Comment(Base):
     __tablename__ = 'comments'
 
     id = Column(Integer, primary_key=True, index=True)
-    blog_id = Column(Integer, ForeignKey('blogs.id', on_delete='CASCADE'), nullable=False)
+    blog_id = Column(Integer, ForeignKey('blogs.id'), nullable=False)
     author = Column(String(100), nullable=False)
     content = Column(Text, nullable=False)
     created = Column(DateTime(timezone=True), server_default=func.now())
