450e8711f5ba57a61bb774d5c9989fe8c41bb487
Shakhobiddin Bozorov <gravitifolz.01@gmail.com>
2024-12-20 11:15:05 +0500

    nothing

--- app/db.py
+++ app/db.py
@@ -7,7 +7,6 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = sqlalchemy.orm.declarative_base()
 
-
 def get_db():
     db = SessionLocal()
     try:
